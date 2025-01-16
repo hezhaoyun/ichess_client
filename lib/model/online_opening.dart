38ef587759a0496ccd50060d69c736bc52cbc32e
照云大大 <hezhaoyun@outlook.com>
2025-01-16 09:52:18 +0800

    online opening cache

--- lib/model/online_opening.dart
+++ lib/model/online_opening.dart
@@ -8,10 +8,31 @@ import 'chess_opening.dart';
 
 part 'online_opening.g.dart';
 
+class OnlineOpeningCache {
+  static final OnlineOpeningCache _instance = OnlineOpeningCache._();
+  factory OnlineOpeningCache() => _instance;
+  OnlineOpeningCache._();
+
+  final Map<String, ChessOpening> cache = {};
+
+  ChessOpening? get(String fen) => cache[fen];
+  void set(String fen, ChessOpening opening) => cache[fen] = opening;
+}
+
 @riverpod
 Future<ChessOpening> onlineOpening(Ref ref, {String fen = ''}) async {
+  // check cache
+  final cachedOpening = OnlineOpeningCache().get(fen);
+  if (cachedOpening != null) return cachedOpening;
+
   final response = await http.get(
     Uri.http('explorer.lichess.ovh', '/master', {if (fen.isNotEmpty) 'fen': fen}),
   );
-  return ChessOpening.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+
+  final chessOpening = ChessOpening.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+
+  // cache result
+  OnlineOpeningCache().set(fen, chessOpening);
+
+  return chessOpening;
 }
