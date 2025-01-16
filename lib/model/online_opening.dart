4261373402fd29a74b17a761e832a48b5f4f1b62
照云大大 <hezhaoyun@outlook.com>
2025-01-16 09:33:08 +0800

    ...

--- lib/model/online_opening.dart
+++ lib/model/online_opening.dart
@@ -11,9 +11,7 @@ part 'online_opening.g.dart';
 @riverpod
 Future<ChessOpening> onlineOpening(Ref ref, {String fen = ''}) async {
   final response = await http.get(
-    Uri.http('explorer.lichess.ovh', '/master', {
-      if (fen.isNotEmpty) 'fen': fen,
-    }),
+    Uri.http('explorer.lichess.ovh', '/master', {if (fen.isNotEmpty) 'fen': fen}),
   );
   return ChessOpening.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
 }
